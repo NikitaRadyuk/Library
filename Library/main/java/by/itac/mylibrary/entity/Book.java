47774195c6630ac678a8828604d50ef4c79478ba
NikitaRadyuk <99745684+NikitaRadyuk@users.noreply.github.com>
2022-06-08 20:44:46 +0300

    Delete Library directory

--- Library/main/java/by/itac/mylibrary/entity/Book.java
+++
@@ -1,5 +0,0 @@
-package by.itac.mylibrary.entity;
-
-public class Book {
-
-}
--- Library/main/java/by/itac/mylibrary/service/StatusBookServcie.java
+++
@@ -1,5 +0,0 @@
-package by.itac.mylibrary.service;
-
-public interface StatusBookServcie {
-	void changeToRead(int id);
-}
